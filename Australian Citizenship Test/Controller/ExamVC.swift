8af1f71050af691a1c0a6d5cc7d702b26d2662c8
Pichai Tangtrong <pichai>
2019-05-07 21:33:21 +1000

    init commit

---
+++ Australian Citizenship Test/Controller/ExamVC.swift
@@ -0,0 +1,25 @@
+//
+//  ExamVC.swift
+//  Australian Citizenship Test
+//
+//  Created by Pichai Tangtrongsakundee on 7/5/19.
+//  Copyright © 2019 Pichai Tangtrongsakundee. All rights reserved.
+//
+
+import UIKit
+
+class ExamVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+
+}
